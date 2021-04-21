9151bb805605494f4ca766898306948362e37b0d
Koichi.Shimomura <unieye51@gmail.com>
2021-04-22 08:35:01 +0900

    2-9: Authenticated User

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -40,4 +40,9 @@ class AuthController extends Controller
            'message' => 'success'
         ])->withCookie($cookie);
     }
+
+    public function user(Request $request)
+    {
+        return $request->user();
+    }
 }
