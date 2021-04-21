d9119f92dba221eb57fa6cf8ae5504bbab7ff557
Koichi.Shimomura <unieye51@gmail.com>
2021-04-22 08:51:06 +0900

    2-11: Scopes

--- routes/api.php
+++ routes/api.php
@@ -20,13 +20,14 @@ Route::prefix('admin')->group(function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
 
-    Route::middleware(['auth:sanctum'])->group(function () {
+    Route::middleware(['auth:sanctum', 'scope.admin'])->group(function () {
         Route::get('user', [AuthController::class, 'user']);
         Route::post('logout', [AuthController::class, 'logout']);
     });
 });
 
-
 // Ambassador
+
+
 
 // Checkout
