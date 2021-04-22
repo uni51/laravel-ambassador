9f87b019a619e0754eab8924a683a63be675e788
Koichi.Shimomura <unieye51@gmail.com>
2021-04-22 09:18:52 +0900

    2-12: Profile

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\UpdateInfoRequest;
+use App\Http\Requests\UpdatePasswordRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -53,5 +55,25 @@ class AuthController extends Controller
         return response([
             'message' => 'success'
         ])->withCookie($cookie);
+    }
+
+    public function updateInfo(UpdateInfoRequest $request)
+    {
+        $user = $request->user();
+
+        $user->update($request->only('first_name', 'last_name', 'email'));
+
+        return response($user, Response::HTTP_ACCEPTED); // 202
+    }
+
+    public function updatePassword(UpdatePasswordRequest $request)
+    {
+        $user = $request->user();
+
+        $user->update([
+            'password' => \Hash::make($request->input('password'))
+        ]);
+
+        return response($user, Response::HTTP_ACCEPTED); // 202
     }
 }
