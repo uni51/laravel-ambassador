320603a8fb91c1140b7a928a379b61301af5757a
Koichi.Shimomura <unieye51@gmail.com>
2021-05-10 10:51:06 +0900

    3-30: Redis Sorted Sets

---
+++ app/Console/Commands/UpdateRankingCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Redis;
+
+class UpdateRankingCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'update:rankings';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $ambassadors = User::ambassadors()->get();
+
+        $bar = $this->output->createProgressBar($ambassadors->count());
+
+        $ambassadors->each(function (User $user) use($bar) {
+            Redis::zadd('rankings', (int)$user->revenue, $user->name);
+
+            $bar->advance();
+        });
+
+        $bar->finish();
+    }
+}
