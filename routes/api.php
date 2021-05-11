4c6a08e968ebbf12168f03cfbe03f55e61a6d6b7
Koichi.Shimomura <unieye51@gmail.com>
2021-05-11 09:19:09 +0900

    4-34: Getting Links

--- routes/api.php
+++ routes/api.php
@@ -61,3 +61,6 @@ Route::prefix('ambassador')->group(function () {
 });
 
 // Checkout
+Route::prefix('checkout')->group(function () {
+    Route::get('links/{code}', [LinkController::class, 'show']);
+});
