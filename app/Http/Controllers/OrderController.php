c446175dcda2ef518365875e3ab63022cef89673
Koichi.Shimomura <unieye51@gmail.com>
2021-05-13 10:06:47 +0900

    4-37: Stripe

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -7,6 +7,7 @@ use App\Models\Link;
 use App\Models\Order;
 use App\Models\OrderItem;
 use App\Models\Product;
+use Cartalyst\Stripe\Stripe;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
@@ -40,6 +41,8 @@ class OrderController extends Controller
 
             $order->save();
 
+            $lineItems = [];
+
             foreach ($request->input('products') as $item) {
                 $product = Product::find($item['product_id']);
 
@@ -52,15 +55,40 @@ class OrderController extends Controller
                 $orderItem->admin_revenue = 0.9 * $product->price * $item['quantity'];
 
                 $orderItem->save();
+
+                $lineItems[] = [
+                    'name' => $product->title,
+                    'description' => $product->description,
+                    'images' => [
+                        $product->image
+                    ],
+                    'amount' => 100 * $product->price,
+                    'currency' => 'usd',
+                    'quantity' => $item['quantity']
+                ];
             }
+
+            $stripe = Stripe::make(env('STRIPE_SECRET'));
+
+            $source = $stripe->checkout()->sessions()->create([
+                'payment_method_types' => ['card'],
+                'line_items' => $lineItems,
+                'success_url' => env('CHECKOUT_URL') . '/success?source={CHECKOUT_SESSION_ID}',
+                'cancel_url' => env('CHECKOUT_URL') . '/error'
+            ]);
+
+            $order->transaction_id = $source['id'];
+            $order->save();
 
             \DB::commit();
 
+            return $source;
         } catch (\Throwable $e) {
             \DB::rollBack();
-            abort(500, 'an error happened');
-        }
 
-        return $order->load('orderItems');
+            return response([
+               'error' => $e->getMessage()
+            ], 400);
+        }
     }
 }
