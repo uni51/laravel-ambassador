7b70c8bd7a3d8a5e430f3ccf4b89c09f2c0d3423
Koichi.Shimomura <unieye51@gmail.com>
2021-05-31 10:36:07 +0900

    modified: for 5-51:Products delete

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-           AmbassadorSeeder::class
+           AmbassadorSeeder::class,
+            ProductSeeder::class,
+            LinkSeeder::class,
+            OrderSeeder::class
         ]);
     }
 }
